d5edc71121093ba2c37b684978a8d2455e5f49da
Harry <Harry@DESKTOP-VM8H4TP>
2026-03-03 19:40:38 +0000

    Resolve merge conflicts in config files for deployment

--- RSVP-App/vite.config.js
+++ RSVP-App/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: '/wedding-rsvp/',
-=======
-  base: '/reception-rsvp-app/',
->>>>>>> e5aeb9c36d898e67e4ce44d874f09981c09cfe0e
   plugins: [react()],
 })
